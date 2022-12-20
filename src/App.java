6eb8c4dca65d287f336fc796f1c9b6331f41c6cc
Lucas Kim <lucaskamadakim@gmail.com>
2022-12-20 16:05:48 -0500

    First commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+        int n = reader.nextInt();
+        System.out.println(n);
+    }
+}
