249717a8fda331133db96c339a0ba0c4de31773e
Elex <elex925@gmail.com>
2022-12-20 16:38:36 -0500

    pain.

--- src/App.java
+++ src/App.java
@@ -3,7 +3,6 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner reader = new Scanner(System.in);
-        int n = reader.nextInt();
-        System.out.println(n);
+        System.out.println(reader.nextInt());
     }
 }
